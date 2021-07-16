b1f8216211ccb8f3e8e125d77261312aaff93fe3
mikhail <kislyakovoffical@gmail.com>
2021-07-16 20:35:19 +0300

    uu

--- index.js
+++ index.js
@@ -41,13 +41,13 @@ const val = async (id) => {
   };
 };
 
-(async () => {
-  let launchOptions = { headless: true, args: ["--start-maximized"] };
+const uslugi = async () => {
+  let launchOptions = { headless: false, args: ["--start-maximized"] };
 
   const browser = await puppeteer.launch(launchOptions);
+
   const page = await browser.newPage();
 
-  // set viewport and user agent (just in case for nice viewing)
   await page.setViewport({ width: 1366, height: 768 });
   await page.setUserAgent(
     "Mozilla/5.0 (X11; Linux x86_64) AppleWebKit/537.36 (KHTML, like Gecko) Chrome/78.0.3904.108 Safari/537.36"
@@ -58,22 +58,45 @@ const val = async (id) => {
     document.querySelector('a[name="currentAction"]').click();
     document.querySelector('span[rel="3"]').click();
   });
+
   const images = await page.$$eval(".captcha-img", (anchors) =>
     [].map.call(anchors, (img) => img.src)
   );
+
   const id = images[0].split("id=")[1];
+
   val(id).then(async (resolved) => {
     const fileInput = await page.$('input[name="docSignature"]');
-    await fileInput.uploadFile("./test.pdf");
+    await fileInput.uploadFile("./files/test.pdf");
     const fileInput2 = await page.$('input[name="docDocument"]');
-    await fileInput2.uploadFile("./test.sig");
-    await page.type("#captchaAnswer3", resolved.captchaAnswer, { delay: 100 });
+    await fileInput2.uploadFile("./files/test.sig");
+    await page.type("#captchaAnswer3", resolved.captchaAnswer, {
+      delay: 100,
+    });
     page.waitForSelector("#elsign-result").then(async () => {
       let element = await page.$("#elsign-result");
       let value = await page.evaluate((el) => el.textContent, element);
       console.log(value);
+      return value;
     });
   });
+};
 
-  await browser.close();
-})();
+const cryptoPro = async () => {
+  let launchOptions = { headless: false, args: ["--start-maximized"] };
+
+  const browser = await puppeteer.launch(launchOptions);
+
+  const page = await browser.newPage();
+
+  await page.setViewport({ width: 1366, height: 768 });
+  await page.setUserAgent(
+    "Mozilla/5.0 (X11; Linux x86_64) AppleWebKit/537.36 (KHTML, like Gecko) Chrome/78.0.3904.108 Safari/537.36"
+  );
+
+  await page.goto("https://www.justsign.me/verifyqca/Verify/");
+};
+
+uslugi().then((val) => {
+  console.log(val);
+});
