818a3ef5025b2a4d9e3d76efc8cf7ec87367e50a
artginzburg <art.ginzburg@ya.ru>
2021-07-18 22:47:31 +0300

    push

--- index.js
+++ index.js
@@ -1,51 +1,47 @@
-const {
-  createWorker
-} = require("tesseract.js");
-const fs = require('fs')
-
+const { createWorker } = require('tesseract.js');
+const fs = require('fs');
 
 const testFolder = './files/';
 
 const worker = createWorker({});
-const puppeteer = require("puppeteer");
+const puppeteer = require('puppeteer');
 
 const resolveCaptcha = async (url) => {
   await worker.load();
-  await worker.loadLanguage("eng");
-  await worker.initialize("eng");
+  await worker.loadLanguage('eng');
+  await worker.initialize('eng');
   await worker.setParameters({
-    tessedit_char_whitelist: "0123456789",
+    tessedit_char_whitelist: '0123456789',
   });
 
   const {
-    data: {
-      text
-    },
+    data: { text },
   } = await worker.recognize(url);
 
-  return '11111';
+  return text;
 };
 
 const getAllFilesNames = () => {
-  let files = fs.readdirSync(testFolder)
-  if ((files.length == 1) && !(files.filter(file => !(['sig', 'pdf'].includes(file.split('.')[1]))).length > 0)) {
-    return 'sig'
-  } else if (files.length == 2 && !(files.filter(file => !(['sig', 'pdf'].includes(file.split('.')[1]))).length > 0)) {
+  let files = fs.readdirSync(testFolder);
+  if (
+    files.length == 1 &&
+    !(files.filter((file) => !['sig', 'pdf'].includes(file.split('.')[1])).length > 0)
+  ) {
+    return 'sig';
+  } else if (
+    files.length == 2 &&
+    !(files.filter((file) => !['sig', 'pdf'].includes(file.split('.')[1])).length > 0)
+  ) {
     console.log('her2');
-    return 'pdfsig'
+    return 'pdfsig';
   } else {
-    return 'tosig'
+    return 'tosig';
   }
-
-}
-
-
+};
 
 const resolved = async (id) => {
   return {
-    captchaAnswer: await resolveCaptcha(
-      "https://www.gosuslugi.ru/pgu/captcha/get?id=" + id
-    ),
+    captchaAnswer: await resolveCaptcha('https://www.gosuslugi.ru/pgu/captcha/get?id=' + id),
     captchaId: id,
   };
 };
@@ -64,149 +60,138 @@ const val = async (id) => {
   };
 };
 
-
 const uslugi = async (count = 1) => {
   if (getAllFilesNames() == 'pdfsig') {
-    {
-      let launchOptions = {
-        headless: false,
-        args: ["--start-maximized"]
-      };
+    let launchOptions = {
+      headless: false,
+      args: ['--start-maximized'],
+    };
 
-      const browser = await puppeteer.launch(launchOptions);
+    const browser = await puppeteer.launch(launchOptions);
 
-      const page = await browser.newPage();
+    const page = await browser.newPage();
 
-      await page.setViewport({
-        width: 1366,
-        height: 768
+    await page.setViewport({
+      width: 1366,
+      height: 768,
+    });
+    await page.setUserAgent(
+      'Mozilla/5.0 (X11; Linux x86_64) AppleWebKit/537.36 (KHTML, like Gecko) Chrome/78.0.3904.108 Safari/537.36'
+    );
+
+    await page.goto('https://www.gosuslugi.ru/pgu/eds');
+    await page.evaluate(() => {
+      document.querySelector('a[name="currentAction"]').click();
+      document.querySelector('span[rel="3"]').click();
+    });
+
+    const images = await page.$$eval('.captcha-img', (anchors) =>
+      [].map.call(anchors, (img) => img.src)
+    );
+
+    const id = images[0].split('id=')[1];
+
+    val(id).then(async (resolved) => {
+      const fileInput = await page.$('input[name="docSignature"]');
+      await fileInput.uploadFile('./files/test.pdf');
+      const fileInput2 = await page.$('input[name="docDocument"]');
+      await fileInput2.uploadFile('./files/test.sig');
+      await page.type('#captchaAnswer3', resolved.captchaAnswer, {
+        delay: 100,
       });
-      await page.setUserAgent(
-        "Mozilla/5.0 (X11; Linux x86_64) AppleWebKit/537.36 (KHTML, like Gecko) Chrome/78.0.3904.108 Safari/537.36"
-      );
 
-      await page.goto("https://www.gosuslugi.ru/pgu/eds");
-      await page.evaluate(() => {
-        document.querySelector('a[name="currentAction"]').click();
-        document.querySelector('span[rel="3"]').click();
-      });
-
-      const images = await page.$$eval(".captcha-img", (anchors) => [].map.call(anchors, (img) => img.src));
-
-      const id = images[0].split("id=")[1];
-
-      val(id).then(async (resolved) => {
-        const fileInput = await page.$('input[name="docSignature"]');
-        await fileInput.uploadFile("./files/test.pdf");
-        const fileInput2 = await page.$('input[name="docDocument"]');
-        await fileInput2.uploadFile("./files/test.sig");
-        await page.type("#captchaAnswer3", resolved.captchaAnswer, {
-          delay: 100,
-        });
-
-        page.waitForSelector("#elsign-result", {
-          timeout: 5000
-        }).then(async () => {
-          let element = await page.$("#elsign-result");
+      page
+        .waitForSelector('#elsign-result', {
+          timeout: 5000,
+        })
+        .then(async () => {
+          let element = await page.$('#elsign-result');
           let value = await page.evaluate((el) => el.textContent, element);
           return value;
-        }).catch(e => {
-          console.log('ОШИИИБКА');
-          uslugi()
         })
-
-
-
-      });
-    }
-  } else {
-    {
-      let launchOptions = {
-        headless: false,
-        args: ["--start-maximized"]
-      };
-
-      const browser = await puppeteer.launch(launchOptions);
-
-      const page = await browser.newPage();
-
-      await page.setViewport({
-        width: 1366,
-        height: 768
-      });
-      await page.setUserAgent(
-        "Mozilla/5.0 (X11; Linux x86_64) AppleWebKit/537.36 (KHTML, like Gecko) Chrome/78.0.3904.108 Safari/537.36"
-      );
-
-      await page.goto("https://www.gosuslugi.ru/pgu/eds");
-      await page.evaluate(() => {
-        document.querySelector('a[name="currentAction"]').click();
-        document.querySelector('span[rel="2"]').click();
-      });
-
-      const images = await page.$$eval(".captcha-img", (anchors) => [].map.call(anchors, (img) => img.src));
-
-      const id = images[0].split("id=")[1];
-
-      val(id).then(async (resolved) => {
-        const fileInput = await page.$('input[name="document"]');
-        await fileInput.uploadFile("./files/test.sig");
-
-        await page.type("#captchaAnswer2", resolved.captchaAnswer, {
-          delay: 100,
+        .catch((e) => {
+          console.log('ОШИИИБКА');
+          uslugi();
         });
+    });
+  } else {
+    let launchOptions = {
+      headless: false,
+      args: ['--start-maximized'],
+    };
 
-        page.waitForSelector("#elsign-result", {
-          timeout: 5000
-        }).then(async () => {
+    const browser = await puppeteer.launch(launchOptions);
 
-          let element = await page.$("#elsign-result");
+    const page = await browser.newPage();
+
+    await page.setViewport({
+      width: 1366,
+      height: 768,
+    });
+    await page.setUserAgent(
+      'Mozilla/5.0 (X11; Linux x86_64) AppleWebKit/537.36 (KHTML, like Gecko) Chrome/78.0.3904.108 Safari/537.36'
+    );
+
+    await page.goto('https://www.gosuslugi.ru/pgu/eds');
+    await page.evaluate(() => {
+      document.querySelector('a[name="currentAction"]').click();
+      document.querySelector('span[rel="2"]').click();
+    });
+
+    const images = await page.$$eval('.captcha-img', (anchors) =>
+      [].map.call(anchors, (img) => img.src)
+    );
+
+    const id = images[0].split('id=')[1];
+
+    return await val(id).then(async (resolved) => {
+      const fileInput = await page.$('input[name="document"]');
+      await fileInput.uploadFile('./files/test.sig');
+
+      await page.type('#captchaAnswer2', resolved.captchaAnswer, {
+        delay: 100,
+      });
+
+      return await page
+        .waitForSelector('#elsign-result', {
+          timeout: 5000,
+        })
+        .then(async () => {
+          let element = await page.$('#elsign-result');
           let value = await page.evaluate((el) => el.textContent, element);
-          if (value.includes('НЕ ПОДТВЕРЖДЕНА')) {
-            await browser.close()
+          const includesAss = value.replace(/\r?\n| /g, '').includes('НЕПОДТВЕРЖДЕНА');
+          if (includesAss) {
+            await browser.close();
+            console.log('подпись говно');
             return {
-              status: false
-            }
+              status: false,
+            };
           } else {
-            await browser.close()
+            await browser.close();
             return {
               status: true,
-              sgn: value.split('Владелец :')[1].split('Издатель')[0]
-            }
-          }
-
-        }).catch(async e => {
-          await browser.close()
-          if (count > 2) {
-            console.log('на базе');
-            return {
-              error: 'Ошибка'
-            }
-          } else {
-            console.log('пидор');
-            uslugi(count + 1).then(val => {
-              return val
-            })
+              sgn: value.split('Владелец :')[1].split('Издатель')[0],
+            };
           }
         })
-
-
-
-      });
-    }
+        .catch(async (e) => {
+          await browser.close();
+          if (count > 1) {
+            console.log('на базе');
+            process.exit('проебался с капчей');
+          } else {
+            console.log('пидор');
+            return await uslugi(count + 1);
+          }
+        });
+    });
   }
-
-
-
-
-}
-
-
+};
 
 const cryptoPro = async () => {
   let launchOptions = {
     headless: false,
-    args: ["--start-maximized"]
+    args: ['--start-maximized'],
   };
 
   const browser = await puppeteer.launch(launchOptions);
@@ -215,15 +200,17 @@ const cryptoPro = async () => {
 
   await page.setViewport({
     width: 1366,
-    height: 768
+    height: 768,
   });
   await page.setUserAgent(
-    "Mozilla/5.0 (X11; Linux x86_64) AppleWebKit/537.36 (KHTML, like Gecko) Chrome/78.0.3904.108 Safari/537.36"
+    'Mozilla/5.0 (X11; Linux x86_64) AppleWebKit/537.36 (KHTML, like Gecko) Chrome/78.0.3904.108 Safari/537.36'
   );
 
-  await page.goto("https://www.justsign.me/verifyqca/Verify/");
+  await page.goto('https://www.justsign.me/verifyqca/Verify/');
 };
 
-uslugi().then((val) => {
-  console.log('val');
-});
+uslugi()
+  .then((val) => {
+    console.log(val);
+  })
+  .catch(console.error);
