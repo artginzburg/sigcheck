97964a8ec10e9e99d9980af35798fb25d5fe4fa9
artginzburg <art.ginzburg@ya.ru>
2021-07-20 22:32:13 +0300

    add express server

--- utils/runParser.js
+++ utils/runParser.js
@@ -1,6 +1,7 @@
 module.exports = async function runParser(parser, browser) {
   try {
     const result = await parser(browser);
+    return result;
     console.log(result);
   } catch (error) {
     console.error(error);
