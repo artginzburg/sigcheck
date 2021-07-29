1f2b48939e04fdf7eff3958c5b06c4ebe65f054b
artginzburg <art.ginzburg@ya.ru>
2021-07-29 23:10:24 +0300

    optimize utils

--- utils/getAllFilesNames.js
+++ utils/getAllFilesNames.js
@@ -1,18 +1,25 @@
 const fs = require('fs');
 
+const resSuffix = 'sig';
+const pdfExt = 'pdf';
+
 module.exports = function getAllFilesNames(pathName) {
   let files = fs.readdirSync(pathName);
-  if (
-    files.length == 1 &&
-    !(files.filter((file) => !['sig', 'pdf'].includes(file.split('.')[1])).length > 0)
-  ) {
-    return 'sig';
-  } else if (
-    files.length == 2 &&
-    !(files.filter((file) => !['sig', 'pdf'].includes(file.split('.')[1])).length > 0)
-  ) {
-    return 'pdfsig';
-  } else {
-    return 'tosig';
-  }
+
+  const filtered = files.filter((file) => ![resSuffix, pdfExt].includes(file.split('.')[1])).length;
+
+  return (
+    (() => {
+      if (!filtered) {
+        if (files.length === 1) {
+          return '';
+        }
+        if (files.length === 2) {
+          return pdfExt;
+        }
+      }
+
+      return 'to';
+    })() + resSuffix
+  );
 };
--- utils/resolveCaptcha.js
+++ utils/resolveCaptcha.js
@@ -1,12 +1,20 @@
 const { createWorker } = require('tesseract.js');
-const worker = createWorker({});
+const worker = createWorker();
+
+const baseUrl = 'https://www.gosuslugi.ru';
+const captchaUrl = new URL('/pgu/captcha/get', baseUrl);
+
+const captchaLength = 5;
+const captchaSymbols = '0123456789';
+
+const workerLanguage = 'eng';
 
 const resolveThroughWorker = async (url) => {
   await worker.load();
-  await worker.loadLanguage('eng');
-  await worker.initialize('eng');
+  await worker.loadLanguage(workerLanguage);
+  await worker.initialize(workerLanguage);
   await worker.setParameters({
-    tessedit_char_whitelist: '0123456789',
+    tessedit_char_whitelist: captchaSymbols,
   });
 
   const {
@@ -16,15 +24,18 @@ const resolveThroughWorker = async (url) => {
   return text;
 };
 
-const resolved = async (id) =>
-  await resolveThroughWorker(`https://www.gosuslugi.ru/pgu/captcha/get?id=${id}`);
+const resolved = async (id) => {
+  const imageToResolve = captchaUrl;
+  imageToResolve.searchParams.set('id', id);
+  return await resolveThroughWorker(imageToResolve.href);
+};
 
 const resolveCaptcha = async (id) => {
   do {
     var captchaAnswer = await resolved(id);
     var res = captchaAnswer.trim();
     captchaAnswer = res + '\n';
-  } while (!(res.length === 5));
+  } while (!(res.length === captchaLength));
 
   return { captchaAnswer };
 };
